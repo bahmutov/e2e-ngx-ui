0075bc663fd2ab77fe76a4f34ed6178e039b2b75
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-15 11:04:50 -0400

    example test for ngx-ui

--- cypress/integration/basic-spec.js
+++ cypress/integration/basic-spec.js
@@ -1,8 +1,26 @@
 describe("ngx-ui basics", () => {
   beforeEach(() => {
-    cy.visit("/")
+    cy.visit("/", {timeout: 10000})
     cy.contains("Angular Style and Component Library")
   })
 
   it("loads", () => {})
+
+  context("Forms", () => {
+    describe("Inputs", () => {
+      it("enters text", () => {
+        cy.get('ul.list-reset')
+          .contains('li', 'Inputs')
+          .find('a')
+          .click()
+        cy.contains('h3', 'Inputs')
+          .should('be.visible')
+        const text = 'hello world'
+        cy.get('input#input-1')
+          .type(text)
+          .blur()
+        cy.contains(`Output: "${text}"`)
+      })
+    })
+  })
 })
