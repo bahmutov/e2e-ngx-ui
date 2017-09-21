df7ede09e8af8d2e402fa5fdf89d9540f134e295
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-21 09:42:37 -0400

    add overlay tests

--- cypress/integration/basic-spec.js
+++ cypress/integration/basic-spec.js
@@ -61,4 +61,41 @@ describe("ngx-ui basics", () => {
       })
     })
   })
+
+  context('Components', () => {
+    describe('Overlay', () => {
+      const gotoOverlay = () => {
+        cy.get('ul.list-reset')
+          .contains('li', 'Overlay')
+          .find('a')
+          .click()
+        cy.contains('h3', 'Overlay')
+          .should('be.visible')
+      }
+
+      const overlayMessage = 'Click anywhere to return'
+
+      beforeEach(gotoOverlay)
+
+      it('Shows overlay', () => {
+        cy.contains('Show Overlay').click()
+        cy.contains(overlayMessage).should('be.visible').click()
+        cy.contains(overlayMessage).should('not.be.visible')
+      })
+
+      it('Handles small screen', () => {
+        cy.contains('Show Overlay').click()
+        cy.viewport(500, 600)
+
+        const message = 'Your browser is too small'
+        cy.contains(message).should('be.visible')
+
+        cy.viewport(1000, 600)
+        cy.contains(message).should('not.be.visible')
+
+        cy.contains(overlayMessage).should('be.visible').click()
+        cy.contains(overlayMessage).should('not.be.visible')
+      })
+    })
+  })
 })
